2f757f80045e72adf049fb0f7d19d4623b7c5637
Rockarya <aryan.jain@students.iiit.ac.in>
2021-10-27 11:33:27 +0530

    spacy vectors with 300 dimensions are added for clickbait dataset

--- src/clickbait/vectorize.py
+++ src/clickbait/vectorize.py
@@ -1,12 +1,15 @@
 import json
 import spacy
 
-nlp = spacy.load("en_core_web_sm")
+nlp = spacy.load("en_core_web_lg")
+
+# for lg the dimensions are 300, checked by adding print statement
 
 if __name__ == '__main__':
-    f = open('../../dataset/clickbait_dataset/processed_string10k.json','r')
+    f = open('../../dataset/clickbait_dataset/processed_string20k.json','r')
     data = json.load(f)
     newData = {}
+    c = 0
     for i in data:
         vectors = nlp(data[i][0])
         newList = []
@@ -15,9 +18,11 @@ if __name__ == '__main__':
         newData[i] = []
         newData[i].append(data[i][1])
         newData[i].append(newList)
+        
+        c += 1
+        print(c)
 
         # print(type(data), type(data[i]))
         # break
-    with open('../../dataset/vectorized_temp.json', 'w') as f:
+    with open('../../dataset/clickbait_dataset/300dims_vectorized_spacy20k.json', 'w') as f:
         json.dump(newData, f)
-        # break
